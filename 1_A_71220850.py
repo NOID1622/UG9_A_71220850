fcb64c2b22557059d31b30eda3de031c5094f029
NOID1622 <112671264+NOID1622@users.noreply.github.com>
2022-11-18 10:27:17 +0700

    Add files via upload

---
+++ 1_A_71220850.py
@@ -0,0 +1,11 @@
+x = "CASSANDRA"
+y = "Praktikum teknologi komputer"
+s = "A"
+print("masukan nama anda:"+(x))
+print("masukan nama mata kuliah:")
+print("masukan grup anda:")
+print('haloo,'+x)
+print("anda tergabung dalam kelas praktikum teknologi komputer pada grup A")
+x = input()
+y = input()
+s = input()
---
+++ 2_A_71220850.py
@@ -0,0 +1,8 @@
+x = "58"
+y = "44"
+s = x - y
+print(s)
+print("suhu dalam skala farenheit:"+(x))
+print(int(x) + "derajat farenheit dikonversi menjadi" + (int(x) - int(y)))
+x = input()
+y = input()
